07cdbd8e91a1a73b64797561bcedea31d774bde1
alexVera99 <alexanderjoel.vera99@gmail.com>
2023-01-31 02:20:48 +0100

    update: define client side message on_ready and on_message

--- src/client.js
+++ src/client.js
@@ -8,11 +8,46 @@ var my_client = {
         my_client.socket.onmessage = my_client.onMessage;
     },
 
+
     sendMessage: function (message) {
       my_client.socket.send(message);
     },
 
     onMessage: function (message) {
+        /*
+        {
+            type: "msg",
+            data: {
+                author_id : 1232,
+                msg: "safdsa"
+            }
+        }
+
+        {
+            type: "connection",
+            data: {
+                author_id: 1234
+            }
+        }
+        */
         console.log(message.data);
-    }
+        var payload = JSON.parse(message.data);
+        var type = payload["type"];
+        var data = payload["data"];
+
+        if (type == "connection" && my_client.on_ready) {
+            var author_id = data["author_id"];
+            my_client.on_ready(author_id);
+        }
+
+        else if (type == "msg" && my_client.on_message) {
+            var author = data["author_id"];
+            var msg = data["msg"];
+            my_client.on_message(author, msg);
+        }
+    },
+
+    /* on_message: function(author_id, msg) {
+        console.log(author_id, msg);
+    } */
 }
