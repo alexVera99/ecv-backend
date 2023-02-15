58ed8d091f3d3e1acd6dad2822a84cd231be7a6e
alexVera99 <alexanderjoel.vera99@gmail.com>
2023-02-15 19:08:44 +0100

    add: base entites of our system

---
+++ src/entities/dataContainers.js
@@ -0,0 +1,80 @@
+export class Animation {
+    avatar_id;
+    image_uri;
+    scale;
+    facing_right;
+    facing_front;
+    facing_left;
+    facing_back;
+    walking_frames;
+    idle_frames;
+    talking_frames;
+
+    fromJSON(data) {
+        this.avatar_id = data["avatar_id"];
+        this.image_uri = data["image_uri"];
+        this.scale = data["scale"];
+        this.facing_right = data["facing_right"];
+        this.facing_front = data["facing_front"];
+        this.facing_left = data["facing_left"];
+        this.facing_back = data["facing_back"];
+        this.walking_frames = data["walking_frames"]
+        this.idle_frames = data["idle_frames"]
+        this.talking_frames = data["talking_frames"]
+    }
+}
+
+export class User {
+    constructor(username) {
+        this.user_id = null;
+        this.username = username || "";
+        this.room_id = null;
+        this.avatar = null;
+        this.facing = null;
+        this.animation = "idle_frames";
+        this.position = null;
+        this.target_position = null;
+    }
+
+    fromJSON(data) {
+        this.user_id = data["user_id"];
+        this.username = data["username"];
+        this.room_id = data["room_id"];
+        this.avatar = data["avatar"];
+        this.facing = data["avatar"]["facing_front"];
+        this.animation = data["animation"];
+        this.position = data["position"];
+        this.target_position = data["target_position"];
+    }
+}
+
+export class Room {
+    constructor(room_name) {
+        this.room_id = null;
+        this.room_name = room_name || "";
+        this.users = [];
+        this.scale = null;
+        this.image_uri = null;
+        this.offset = null;
+        this.range = null;
+        this.exits = null;
+    }
+
+    fromJSON(data) {
+        this.room_id = data["room_id"];
+        this.room_name = data["room_name"];
+        this.users = data["users"];
+        this.scale = data["scale"]
+        this.image_uri = data["image_uri"];
+        this.offset= data["offset"];
+        this.range = data["range"];
+        this.exits = data["exits"].map((exit) => {
+            return {
+                position: exit.position,
+                height: exit.height,
+                width: exit.width,
+                to_room_id:exit.to_room_id
+            };
+        })
+    }
+}
---
+++ src/entities/dataManager.js
@@ -0,0 +1,65 @@
+export class World{
+    constructor() {
+        this.users = new Map();
+        this.rooms = new Map();
+        this.animations = new Map();
+    }
+
+    getUser(user_id){
+        return this.users.get(user_id);
+    }
+
+    getAllUsersInRoom(room_id) {
+        return this.rooms.set(room_id, users);
+    }
+
+    getRoom(room_id){
+        return this.rooms.get(room_id);
+    }
+
+    getAnimation(id){
+        return this.animations.get(id);
+    }
+    
+    addUser(user){
+        var user_id = user_id;
+
+        this.users.set(user_id, user);
+    }
+
+    addUserToRoom(user, room_id){
+        var user_id = user_id;
+
+        this.rooms.get(room_id).users.set(user_id, user);
+    }
+
+    addRoom(room){
+        this.rooms.set(room.room_id, room);
+    }
+
+    addAnimation(animation){
+        this.animations.set(animation.avatar_id, animation);
+    }
+
+    changeRoom(user_id, room_id){
+        var room = this.getRoom(room_id);
+        var user = this.getUser(user_id);
+        
+        room.users.delete(user_id);
+        
+        user.room_id = room_id;
+
+        var new_room = this.getRoom(room_id);
+        new_room.users.set(this.currentUserId, user);
+    }
+
+    removeUser(user_id) {
+        var user = this.getUser(user_id);
+        var room_id = user.room_id;
+        // Delete from users list
+        this.users.delete(user_id)
+        // Delete from users list of the room
+        this.rooms.get(room_id).users.delete(user_id);
+    }
+
+}
