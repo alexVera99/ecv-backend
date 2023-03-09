f701783d45fedcbd9b46b92007fd029d652118a7
alexVera99 <alexanderjoel.vera99@gmail.com>
2023-03-09 18:29:38 +0100

    remove unneeded code

--- src/repository/MySQL/roomRepository.js
+++ src/repository/MySQL/roomRepository.js
@@ -6,42 +6,6 @@ export class RoomRepository extends IRoomRepository {
         super();
         this.table = "MOONSCAPE_3D_rooms";
         this.connector = connector;
-        this.room1Data = {
-            room_id: 1,
-            scale: 2.5,
-            room_name: "street1",
-            users: new Map(),
-            image_uri: "../imgs/bg1.png",
-            offset: 0,
-            range: [-200, 200],
-            exits: [
-                {
-                    position: [364, 125],
-                    height:41,
-                    width:24,
-                    to_room_id: 2
-                }
-            ]
-        };
-        
-        this.room2Data = {
-            room_id:2,
-            scale: 2.05,
-            room_name:"street2",
-            users: new Map(),
-            image_uri: "../imgs/city.png",
-            offset: 0,
-            range: [-300, 300],
-            exits: [
-                {
-                    position:[518, 164],
-                    height: 35,
-                    width: 20,
-                    to_room_id:1
-                }
-            ]
-        
-        };
     }
 
     async getRoomById(id) {
@@ -55,7 +19,6 @@ export class RoomRepository extends IRoomRepository {
                         
 
         return room;
-        //return this.room1Data;
     }
 
 
